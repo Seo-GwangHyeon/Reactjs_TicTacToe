4dec4bae5a30aaf9c3a91b06cc198f91c906d0e5
GwanghyeonSeo <dya2122@gmail.com>
2020-01-30 13:46:39 -0600

    1M20C 기본 틱택토 완성
    기본 틱택토 완성

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,24 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
-    }
+function Square(props) {
+    //controlled components. 
+    return (// 버튼의 value를 보여지는 값으로 설정
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component{
-    renderSquare(i)
+    constructor(props)
     {
-        return <Square value={i}/>;
+        super(props);//하위 클래스의 생성자를 정의할 때 항상 super를 호출 해야한다. 
+        // 상위 클래스의 생성자를 사용하는 것? 
+        this.state ={
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();//배열의 사본을 만든다.
+        /* 이유 ㅣ
+            - history를 알 수 있어, 복잡한 특징들을 단순하게 구현할 수 있게한다( 시간여행)
+            - 변화를 감지하여 기존 객체와 비교하여 변화를 감지할 수 있다.
+            - 렌더링 시기를 결정할 수 있다.
+        */
+        if (calculateWinner(squares) || squares[i])
+        {
+            return;
+        }
+
+        squares[i]=this.state.xIsNext? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i)
+    {//Square의 value를 전달
+        return (
+        <Square value={this.state.squares[i] }
+        onClick={() => this.handleClick(i)}// handle클릭은 임의로 붙인 이름
+        />
+        );
     }
 
     render(){
-        const status = 'Next Player: X';
+        const winner = calculateWinner(this.state.squares);
+
+        let status;
+        if (winner){
+            status='Winner : '+winner;
+        }
+        else
+        {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -64,3 +104,23 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
